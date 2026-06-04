81a55f179352bc65409baf49fe0d3ff7127b8cca
julianoarqui <juliianoarqui@gmail.com>
2026-06-04 10:40:12 -0300

    cometado linha devido a duplicidade

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 30;
+//let numeroLimite = 30;  --comentado pois estava errado
 let numeroLimite = 25;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
